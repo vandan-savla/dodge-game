1f93d6c332eb13fb2f1ba572461d8939846707bf
vandan savla <74498329+vandan-savla@users.noreply.github.com>
2020-11-16 13:09:51 +0530

    Add files via upload

---
+++ dino.js
@@ -0,0 +1,25 @@
+var character= document.getElementById('character');
+var block= document.getElementById('block');
+var counter=0;
+function jump() {
+ character.classList.add("animate");
+ setTimeout(function(){
+     character.classList.remove("animate");
+ },500);
+}
+var dead=setInterval(function (){
+    var charTop= parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+   
+    if(blockLeft<20 &&  blockLeft>-20  && charTop>=130)
+   {
+       block.style.animation="none";
+       alert("game over || your score: "+Math.floor(counter/100));
+       counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }
+    else {
+        counter++;
+        document.getElementById('score').innerHTML = Math.floor(counter / 100);
+    }
+},10);
